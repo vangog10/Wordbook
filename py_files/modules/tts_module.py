3f445d235efe2a09a4ba8912e604e1d6ea1d876e
79224 <igvazdovskij@gmail.com>
2023-11-03 20:12:32 +0500

    update

--- py_files/modules/tts_module.py
+++ py_files/modules/tts_module.py
@@ -1,9 +1,16 @@
-import pyttsx3
+from gtts import gTTS
+import os
+
+
+CASHE = {}
+MP3_PATH = 'F:/Projects/PycharmProjects/WordBook/mp3_cashe/{}.mp3'
 
 
 def speak(word: str):
-    engine = pyttsx3.init()
-    engine.say(word)
-    engine.runAndWait()
+    if word not in CASHE:
+        tts = gTTS(word, lang='en')
+        tts.save(MP3_PATH.format(word))
+        CASHE[word] = MP3_PATH.format(word)
+    os.system(f'afplay {MP3_PATH.format(word)}')
 
 
